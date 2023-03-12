01c8d6801dd1d300d016b6d1c5fc2c623a9c5f0f
attotem <vshinkarenko21@gmail.com>
2023-03-12 05:04:56 +0200

    suka

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Components/Card';
-// import powerbank from './Photos/pw.png'
-// import iqos from 'src/iqos.png'
-// import vape from 'src/vape.png'
-// import nag from 'src/nag.png'
+import powerbank from './Photos/pw.png'
+import iqos from './Photos/iqos.png'
+import vape from './Photos/vape.png'
+import nag from './Photos/nag.png'
 import glo from './Photos/glo.png'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Card img={glo} title="Ситсеми нагрівання тютюну"/>
-   <Card img={glo} title="Iqos"/>
-   <Card img={glo} title="GLO"/>
-   <Card img={glo} title="JOUZ"/>
-   <Card img={glo} title="Павербанки та зар. станції"/>
+   <Card img ={nag} title="Ситсеми нагрівання тютюну"/>
+   <Card img ={iqos} title="Iqos"/>
+   <Card img ={glo} title="GLO"/>
+   <Card img ={vape} title="JOUZ"/>
+   <Card img ={powerbank}  title="Павербанки та зар. станції"/>
   </React.StrictMode>
 );
 
