d38de8052383e28a89cfc64ce96931365424e718
attotem <vshinkarenko21@gmail.com>
2023-03-12 04:41:21 +0200

    glo

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Components/Card';
 // import powerbank from './Photos/pw.png'
-import iqos from './iqos.png'
-import vape from './vape.png'
-import nag from './nag.png'
-import glo from './glo.png'
+// import iqos from 'src/iqos.png'
+// import vape from 'src/vape.png'
+// import nag from 'src/nag.png'
+import glo from '/Pods/pods/src/glo.png'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Card img ={nag} title="Ситсеми нагрівання тютюну"/>
-   <Card img ={iqos} title="Iqos"/>
+   <Card img ={glo} title="Ситсеми нагрівання тютюну"/>
+   <Card img ={glo} title="Iqos"/>
    <Card img ={glo} title="GLO"/>
-   <Card img ={vape} title="JOUZ"/>
-   <Card img ={iqos}  title="Павербанки та зар. станції"/>
+   <Card img ={glo} title="JOUZ"/>
+   <Card img ={glo}  title="Павербанки та зар. станції"/>
   </React.StrictMode>
 );
 
