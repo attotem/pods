e4eec9f62f0500fb5b13f12035074c1f4729186f
lostconsciousness <general2286@gmail.com>
2023-03-13 07:04:55 +0200

    2nd page and nasrano

---
+++ src/pages/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+const Heater = () =>{
+//   for(let i = 0; i < 5; i++){
+//     a[i] =  (
+//          //<CatalogCard picture={json.picture[0]} name={json_content[i].name} price={json_content[i].price} currencyId={json_content[i].currencyId} />
+//           <h1>dsfds</h1>
+//       );
+// }
+// return a;
+// };
+  
+// export default Heater;
